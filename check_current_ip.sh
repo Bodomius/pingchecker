74f05a54e28a5840de2eb737f4edf7db839d7153
Bodomius <80171683+Bodomius@users.noreply.github.com>
2025-04-22 23:39:07 +0300

    Update check_current_ip.sh

--- check_current_ip.sh
+++ check_current_ip.sh
@@ -3,7 +3,7 @@
 # === НАСТРОЙКИ ===
 BOT_TOKEN="YOUR_BOT_TOKEN"
 CHAT_ID="YOUR_CHAT_ID"
-SERVER_NAME="SERVERNAME"
+SERVERNAME="SERVER_NAME"
 API_URL="https://api.telegram.org/bot$BOT_TOKEN/sendMessage"
 PING_CMD="/bin/ping"
 LOG_FILE="/tmp/ping_debug.log"
